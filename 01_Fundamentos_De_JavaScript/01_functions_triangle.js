406a9a7b7c441a339d2eb1e507819d8d8ec1996d
Julio Jaramillo <juliocesar0293@gmail.com>
2017-11-02 13:43:22 -0600

    Initial Commit

---
+++ 01_Fundamentos_De_JavaScript/01_functions_triangle.js
@@ -0,0 +1,8 @@
+let base = 5;
+let altura = 7;
+
+const triangleArea =  (base, altura) =>{ base *  altura / 2 };
+
+console.log(`El area de un trianglo de base ${base} y altura ${altura} = ${ triangleArea(base, altura) } `);
+
+

499c46a567114bc710e10f00c89a5482eaffef0a
Julio Jaramillo <juliodev@mx1.ibm.com>
2020-01-11 16:26:40 -0600

    Integracion de Express con React

---
+++ ServerSideRender/PlatziVideo/src/server/server.js
@@ -0,0 +1,53 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import webpack from 'webpack';
+
+dotenv.config();
+
+const ENV = process.env.NODE_ENV;
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+if (ENV === 'development') {
+  console.log('Loading dev config');
+  const webpackConfig = require('../../webpack.config');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const compiler = webpack(webpackConfig);
+  const serverConfig = {
+    contentBase: `http://localhost${PORT}`,
+    port: PORT,
+    publicPath: webpackConfig.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    states: {
+      colors: true,
+    },
+  };
+
+  app.use(webpackDevMiddleware(compiler, serverConfig));
+  app.use(webpackHotMiddleware(compiler));
+}
+
+app.get('*', (req, res) => {
+  res.send(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+          <title>Platzi Video</title>
+          <link rel="stylesheet" href="assets/app.css" type="text/css"/>
+      </head>
+      <body>
+          <div id="app"></div>
+          <script src="assets/app.js" type="text/javascript"></script>
+          <script src="assets/vendor.js" type="text/javascript"></script>
+      </body>
+    </html>
+  `);
+});
+
+app.listen(PORT, (err) => {
+  if (err) console.log(err);
+  console.log(`Server running on port ${PORT}`);
+});

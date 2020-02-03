40c032c9414bb2f235730ee7b75f601bb6985e90
Julio Jaramillo <juliodev@mx1.ibm.com>
2020-02-02 18:33:37 -0600

    Aplicar Server Side Render

---
+++ EscuelaPlatziJavascript/ServerSideRender/PlatziVideo/src/frontend/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {hydrate} from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, compose } from 'redux';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+import reducer from './reducers';
+import App from './routes/App';
+/*import initialState from './initialState'*/
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const preloadedState = window.__PRELOADED_STATE__;
+const store = createStore(reducer, preloadedState, composeEnhancers());
+const history = createBrowserHistory();
+
+
+hydrate (
+  <Provider store={store}>
+    <Router history={history}>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('app'),
+);
